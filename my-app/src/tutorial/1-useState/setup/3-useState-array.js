02672f4b49211f584f262ef8c17314f69edd834a
Srishti Parti <srishtiparti97@gmail.com>
2022-10-17 21:11:44 -0400

    useState with arrays and objects

--- my-app/src/tutorial/1-useState/setup/3-useState-array.js
+++ my-app/src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,7 +2,34 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people,setPeople] = React.useState(data)
+
+  const removeItem =(id)=>{
+    let newPeople =people.filter((person)=>{
+      if(person.id!==id){
+      return person
+      }
+      
+    })
+    setPeople(newPeople)
+  }
+
+  return (
+    <>
+    {
+      people.map((person)=>{
+        const {id,name} = person
+        return (
+          <div key={id} className ='item'>
+            <h4>{name}</h4>
+            <button className='btn' onClick={()=>removeItem(id)} >Remove Item</button>
+          </div>
+        )
+      })
+    }
+    <button type='button' className='btn' onClick={()=>setPeople([])}>Clear All Items</button>
+    </>
+  );
 };
 
 export default UseStateArray;
--- my-app/src/tutorial/1-useState/setup/4-useState-object.js
+++ my-app/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,31 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person,setPerson] = useState({
+  name:'people',
+  age:24,
+  message:'this is the message'
+})
+
+const [name,setName] = useState('peter')
+const [age, setAge] = useState('24')
+const [message, setMessage] = useState('this is a message')
+
+const changeMessage =()=>{
+ // setPerson({...person,message:'hello world'})
+ setMessage('Hello World')
+}
+  return (
+    <>
+    {/* <h1>{person.name}</h1>
+    <h2>{person.age}</h2>
+    <h3>{person.message}</h3> */}
+    <h1>{name}</h1>
+    <h2>{age}</h2>
+    <h3>{message}</h3>
+    <button className='btn' onClick={changeMessage}>change Message</button>
+    </>
+  );
 };
 
 export default UseStateObject;
